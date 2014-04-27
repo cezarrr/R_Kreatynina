abad97166dd5368fa02174faf0dab795382a6b6c
CJ <cezarrr@gmail.com>
2014-04-28 00:45:22 +0200

    Trwaja prace nad liczeniem wynikow dla pozostalych poziomow kreatyniny

--- GetWhichHighestNumber.R
+++ GetWhichHighestNumber.R
@@ -5,6 +5,9 @@
 GetWhichHighestNumber<-function (df){
   toReturn<-as.data.frame(setNames(replicate(length(colnames(df)),numeric(0), simplify = F), colnames(df)))
   for (i in 1:length(colnames(df))){
+    if(any(is.na(df[,i]))){
+      toReturn[1,i]<-"NA value"
+    }
     toReturn[1,i]<-row.names(df)[which.max(df[,i])]
   }
   return (toReturn)
--- kreatynina.r
+++ kreatynina.r
@@ -1,8 +1,16 @@
 #!/usr/bin/env Rscript
 
-#zmienna do edytowania - parametr n w walidacji krzyzowej
+#####zmienne do edytowania#####
+
+#parametr n w walidacji krzyzowej
 #ma najwiekszy wplyw na czas wykonania skryptu
-cv_n = 5
+cv_n = 1
+
+#czy liczyc dodatkowo regresje (z tuningiem) dla wszystkich atrybutow
+#bardzo powolna operacja z relatywnie slabymi wynikami
+useAllAttributess=FALSE
+
+#####koniec zmiennych do (czestego) edytowania######
 
 #dolaczamy biblioteki
 library(dmr.regeval)
@@ -33,6 +41,9 @@ reduced_base<- df_base[, -which(names(df_base) %in% kreat_names)]
 
 #ustalamy kilka "naiwnych" licznosci atrybutow
 n_lengths <- c(2:20,25,30,40,50,60,70,80,90)
+
+#tworzy katalog na wykresy
+dir.create(file.path("plots"),showWarnings=FALSE)
 
 #szkic histogramu wartosci kreat_1
 png(paste("plots/","hist_kreat_1",".png",sep=""),width=600,height=600)
@@ -72,6 +83,10 @@ db_all_cv10_r2<- dmr.regeval::r2(db_all_cv10$pred, db_all_cv10$true)
 mse(db_all_cv10$pred, db_all_cv10$true)
 
 #metody selekcji atrybutow
+#usuwamy i tworzymy na nowo, zeby uniknac starych wartosci
+if(exists("namesOfTests")){
+  rm(namesOfTests)
+}
 namesOfTests <- c("linear.correlation","rank.correlation","random.forest")
 
 
@@ -98,7 +113,7 @@ for (algor in namesOfTests){
   for (i in n_lengths){
     sub_set <- cutoff.k(weights[[algor]], i)
     sub_base <- reduced_base[, which(names(reduced_base) %in% c(sub_set,"kreat_1"))]
-    testRes<-dmr.claseval::crossval(rpart,kreat_1~.,as.data.frame(impute(sub_base,what="median"),k=10,n=cv_n)
+    testRes<-dmr.claseval::crossval(rpart,kreat_1~.,as.data.frame(impute(sub_base,what="median")),k=10,n=cv_n)
     selectionResults[toString(i),algor]<-dmr.regeval::r2(testRes$pred, testRes$true)
     #sprawdzmy takze dodatkowo korelacje przewidywanych wynikow z rzeczywistymi
     selectionResultsR2[toString(i),algor]<-cor(testRes$pred, testRes$true)
@@ -119,7 +134,7 @@ for (algor in namesOfTests){
   png(paste("plots/","RegCor_",algor,".png",sep=""),width=600,height=600)
   plot(n_lengths,selectionResultsR2[,algor],
        type="o",xlab="Moc podzbioru atrybutow",ylab="Korelacja prawdziwych oraz przewidywanych wartosci funkcji docelowej",
-       main=paste("Zaleznosc korelacji prawdziwych oraz przewidywanych wartosci funkcji docelowej determinacji \nod mocy podzbioru atrybutow dla ",algor,sep=""))
+       main=paste("Zaleznosc korelacji prawdziwych oraz przewidywanych wartosci funkcji docelowej \nod mocy podzbioru atrybutow dla ",algor,sep=""))
   dev.off()
 }
 
@@ -136,27 +151,37 @@ for (t in namesOfTests){
 }
 #dodajemy takze atrybuty z cfs
 selectedSubsets[["cfs"]]<- CreateDfFromAttributes(reduced_base,cfsAtts,"kreat_1")
-
-extendedNamesOfTests<-c(namesOfTests,"cfs")
+namesOfTests<-c(namesOfTests,"cfs")
 
 #rysujemy korelogramy dla wybranych metod selekcji atrybutow 
-for(corGraph in extendedNamesOfTests){
+for(corGraph in namesOfTests){
   png(paste("plots/","corrgram",corGraph,".png",sep=""),width=600,height=600)
   DrawSubsetCorrgram(selectedSubsets[[corGraph]],corGraph)
   dev.off()
 }
 
+#dodajemy pelna baze jako punkt odniesienia
+if(useAllAttributes==TRUE){
+  selectedSubsets[["all.attributes"]] <-reduced_base 
+  namesOfTests<-c(namesOfTests,"all.attributes")
+}
+#tworzymy struktury, potrzebne do testu regresji
 regressionResults<-list()
-regressionR2<-as.data.frame(setNames(replicate(length(extendedNamesOfTests),numeric(0), simplify = F), extendedNamesOfTests))
-#regressionCor<-as.data.frame(setNames(replicate(length(extendedNamesOfTests),numeric(0), simplify = F), extendedNamesOfTests))
+regressionR2<-as.data.frame(setNames(replicate(length(namesOfTests),numeric(0), simplify = F), namesOfTests))
+bestR2<- -5
 
-for(selection in extendedNamesOfTests){
+#przeprowadzamy tuning modeli (wraz z ich ocena)
+for(selection in namesOfTests){
   #regression tree
   tmpResults<-tune(rpart,kreat_1~.,data=as.data.frame(impute(selectedSubsets[[selection]]),what="median"),ranges = list(minsplit = c(5,10,20,30,40,50,60,80),cp=c(0.01,0.05,0.08,0.1,0.15)),
                    tunecontrol=tune.control(nrepeat=cv_n, repeat.aggregate=mean,sampling="cross", cross=10))
   tmpR2 <- (1 -( length(selectedSubsets[[selection]]$kreat_1)*tmpResults$best.performance/((length(selectedSubsets[[selection]]$kreat_1)-1)*var(selectedSubsets[[selection]]$kreat_1))))
   print(paste(selection," regression tree: ",tmpR2))
   regressionResults[[selection]][["regression tree"]] <- tmpResults
+  if(bestR2<tmpR2){
+    bestR2<-tmpR2
+    bestModel <- tmpResults
+  }
   regressionR2["regression tree",selection] <- tmpR2
 
   #linear regression
@@ -166,17 +191,68 @@ for(selection in extendedNamesOfTests){
   tmpR2 <- (1 -( length(selectedSubsets[[selection]]$kreat_1)*tmpResults$best.performance/((length(selectedSubsets[[selection]]$kreat_1)-1)*var(selectedSubsets[[selection]]$kreat_1))))
   print(paste(selection," linear regression: ",tmpR2))
   regressionResults[[selection]][["linear regression"]] <- tmpResults
+  if(bestR2<tmpR2){
+    bestR2<-tmpR2
+    bestModel <- tmpResults
+  }
   regressionR2["linear regression",selection] <- tmpR2
   
-  #support vector regression
-  tmpResults<-tune(svm,kreat_1~.,data=as.data.frame(impute(selectedSubsets[[selection]]),what="median"), ranges = list(gamma = 2^(-2:1), cost = 2^(1:4)),
+  #support vector regression - radial kernel
+  tmpResults<-tune(svm,kreat_1~., data=as.data.frame(impute(selectedSubsets[[selection]]),what="median"), ranges = list(gamma = 2^(-5:1), cost = 2^(-2:1),kernel=c("polynomial","radial")),
                    tunecontrol=tune.control(nrepeat=cv_n, repeat.aggregate=mean,sampling="cross", cross=10))
   
   tmpR2 <- (1 -( length(selectedSubsets[[selection]]$kreat_1)*tmpResults$best.performance/((length(selectedSubsets[[selection]]$kreat_1)-1)*var(selectedSubsets[[selection]]$kreat_1))))
   print(paste(selection," svr: ",tmpR2))
   regressionResults[[selection]][["svr"]] <- tmpResults
+  if(bestR2<tmpR2){
+    bestR2<-tmpR2
+    bestModel <- tmpResults
+  }
   regressionR2["svr",selection] <- tmpR2
   
 }
+
+#zapis wynikow do pliku, do pozniejszej analizy
+dir.create(file.path("results"),showWarnings=FALSE)
+resultsFile <- file("results/regressionResults.txt", open = "wt")
+sink(resultsFile)
+print(regressionResults)
+
+close(resultsFile)
+#powrot do zwyklego ujscia
+sink(type = "message")
+sink()
+
+#wyniki R2
+write.csv(regressionR2,"results/regressionR2.csv")
+
+bestRegressionResultIndex<- which(regressionR2== max(regressionR2,na.rm=TRUE), arr.ind = TRUE)
+
+#wprowadzamy wektor nazw modeli
+modelNames<- c("regression tree","linear regression","svr")
+
+bestRegressionDataFrame <- as.data.frame(selectedSubsets[[namesOfTests[bestRegressionResultIndex[2]]]])
+bestRegressionModel <- modelNames[bestRegressionResultIndex[1]]
+bestParams<- regressionResults[namesOfTests[bestRegressionResultIndex[2]]]
+
+testModels <-vector()
+if(bestRegressionModel=="svr"){
+  model1 <- svm(kreat_1~.,data=bestRegressionDataFrame,gamma=bestModel$best.parameters$gamma,cost=bestModel$best.parameters$cost,kernel=bestModel$best.parameters$kernel)
+  testModel1<-tune(svm,kreat_1~.,data=bestRegressionDataFrame,gamma=bestModel$best.parameters$gamma,cost=bestModel$best.parameters$cost,kernel=bestModel$best.parameters$kernel,
+                   tunecontrol=tune.control(nrepeat=cv_n, repeat.aggregate=mean,sampling="cross", cross=10))
+  testModels<- c(testModel1$best.performance)
+ 
+  for(kreat in kreat_names){
+    
+    kreatBase <- bestRegressionDataFrame[, -which(names(bestRegressionDataFrame) %in% kreat1_name)]
+    kreatBase <- CreateDfFromAttributes(fullDF=df_base,atts=colnames(kreatBase),additionalAtt=kreat)
+    testModel<- tune(svm,as.formula(paste(kreat,"~.")),data=kreatBase,gamma=bestModel$best.parameters$gamma,cost=bestModel$best.parameters$cost,kernel=bestModel$best.parameters$kernel,
+                   tunecontrol=tune.control(nrepeat=cv_n, repeat.aggregate=mean,sampling="cross", cross=10))
+    testModels<-c(testModels,testModel$best.performance)
+  }
+}
+# else if(bestRegressionModel=="regression tree"){
+#   model1<- rpart(kreat_1~.,data=bestRegressionDataFrame,minsplit=kreat_1~.,bestModel$best.parameters$minsplit,cp=bestModel$best.parameters$cp)
+# }
 
 
